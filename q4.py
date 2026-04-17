f12150389987dfedbb713719dee010dd3277c67f
nkatine2026 <nkatine2026@kehillahstudent.org>
2026-04-17 11:25:37 -0700

    itwork!

--- q4.py
+++ q4.py
@@ -1,12 +1,28 @@
 import turtle
+constant = 2
+c1 = 1.2
+c2 = 1.4
 class Point:
     def __init__ (self, x, y):
         self.x = x
         self.y = y
+        self.w = x*constant
+        self.z = x*constant
     def draw(self):
         turtle.goto(self.x, self.y)
+    def scale(self, constant):
+        self.x = self.x * constant
+        self.y = self.y * constant
+    def draw2(self):
+        turtle.goto(self.w, self.z)
     def __str__(self):
         return f"({self.x}, {self.y})"
+    def sheer(self):
+        self.wa = self.x*c1
+        self.za = self.y*c2
+    def drawboogalo(self):
+        turtle.goto(self.wa, self.za)
+    
 p1 = Point(0,0)
 p2 = Point(100, 0)
 p3 = Point(100, 100)
@@ -15,8 +31,14 @@ p2.draw()
 p3.draw()
 p4.draw()
 p1.draw()
-
-
+p2.sheer()
+p3.sheer()
+p4.sheer()
+p1.sheer()
+p2.drawboogalo()
+p3.drawboogalo()
+p4.drawboogalo()
+p1.drawboogalo()
 
 
 
